d4356943cdd93cf729ede6e9acf1d96939740aa4
WeAreTheException <trexpronto@gmail.com>
2026-04-16 19:39:03 -0700

    Tugga

--- Tugga/BattleScale.gd
+++ Tugga/BattleScale.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name BattleScale
 
+signal scale_changed(value: int)
+
 @export var win_value: int = 5
 
 var current_value: int = 0
@@ -10,6 +12,10 @@ func add_direct_damage(amount: int, owner: int) -> void:
 		current_value += amount
 	elif owner == Card.Owner.OPPONENT:
 		current_value -= amount
+
+	current_value = clamp(current_value, -win_value, win_value)
+
+	scale_changed.emit(current_value)
 
 	print("scale: ", current_value)
 
