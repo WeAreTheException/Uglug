6f02668651af19909e4ba1eb8f7629123c4b86d8
WeAreTheException <trexpronto@gmail.com>
2026-05-11 16:47:23 -0700

    Made it work again. png were mssing

--- BuffPileButton.gd
+++ BuffPileButton.gd
@@ -9,17 +9,34 @@ var buff_database: BuffDatabase = null
 func _ready() -> void:
 	buff_database = get_node_or_null("BuffDatabase") as BuffDatabase
 
+	if spawn_anchor == null:
+		spawn_anchor = find_child("BuffInstanceSpawnAncho", true, false) as Node2D
+
+	if spawn_anchor == null:
+		spawn_anchor = find_child("BuffInstanceSpawnAnchor", true, false) as Node2D
+
+	print("BuffPile ready on: ", get_path())
+	print("mutation_scene = ", mutation_scene)
+	print("spawn_anchor = ", spawn_anchor)
+	print("buff_database = ", buff_database)
+
 func _pressed() -> void:
+	if spawn_anchor == null:
+		spawn_anchor = find_child("BuffInstanceSpawnAncho", true, false) as Node2D
+
+	if spawn_anchor == null:
+		spawn_anchor = find_child("BuffInstanceSpawnAnchor", true, false) as Node2D
+
 	if buff_database == null:
-		print("buff pile blocked: buff_database is null")
+		print("buff pile blocked: buff_database is null on ", get_path())
 		return
 
 	if mutation_scene == null:
-		print("buff pile blocked: mutation_scene is null")
+		print("buff pile blocked: mutation_scene is null on ", get_path())
 		return
 
 	if spawn_anchor == null:
-		print("buff pile blocked: spawn_anchor is null")
+		print("buff pile blocked: spawn_anchor is null on ", get_path())
 		return
 
 	var mutation := buff_database.get_random_mutation()
