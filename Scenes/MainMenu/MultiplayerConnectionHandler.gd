bfbe509fea88d44f6109ae9a21afe6ba98c7a9cd
Sergio Lisuardi <13lisuardi@gmail.com>
2026-06-10 19:32:22 -0700

    Fixing duplicate usernames

--- Scenes/MainMenu/MultiplayerConnectionHandler.gd
+++ Scenes/MainMenu/MultiplayerConnectionHandler.gd
@@ -5,10 +5,14 @@ signal connection_started
 signal connection_succeeded
 signal connection_failed(error: int)
 
+@export var force_connection_failure := false
+
 var is_connected_to_gdsync := false
 var is_connecting := false
 
-@export var force_connection_failure := false
+
+func _ready() -> void:
+	randomize()
 
 
 func start_connection() -> void:
@@ -35,6 +39,14 @@ func start_connection() -> void:
 func _on_connected() -> void:
 	is_connecting = false
 	is_connected_to_gdsync = true
+
+	var username := _build_unique_username()
+	GDSync.player_set_username(username)
+
+	print("GD-Sync connected.")
+	print("Client ID: ", GDSync.get_client_id())
+	print("Username set to: ", username)
+
 	connection_succeeded.emit()
 
 
@@ -42,3 +54,10 @@ func _on_connection_failed(error: int) -> void:
 	is_connecting = false
 	is_connected_to_gdsync = false
 	connection_failed.emit(error)
+
+
+func _build_unique_username() -> String:
+	var client_id := str(GDSync.get_client_id())
+	var random_id := str(randi_range(1000, 9999))
+
+	return "Player" + client_id + random_id
