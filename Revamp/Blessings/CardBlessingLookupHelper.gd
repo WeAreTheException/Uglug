b4e4c2e41d1207d91a72338d3b7403b5d921a25a
WeAreTheException <trexpronto@gmail.com>
2026-06-11 14:23:43 -0700

    cards are blessed in debug

--- Revamp/Blessings/CardBlessingLookupHelper.gd
+++ Revamp/Blessings/CardBlessingLookupHelper.gd
@@ -6,10 +6,22 @@ func get_card_blessings(card: CardRoot) -> CardBlessings:
 	if card == null:
 		return null
 
-	for child in card.get_children():
-		var card_blessings := child as CardBlessings
+	return _find_card_blessings_recursive(card)
 
-		if card_blessings != null:
-			return card_blessings
+
+func _find_card_blessings_recursive(node: Node) -> CardBlessings:
+	if node == null:
+		return null
+
+	var card_blessings := node as CardBlessings
+
+	if card_blessings != null:
+		return card_blessings
+
+	for child in node.get_children():
+		var found := _find_card_blessings_recursive(child)
+
+		if found != null:
+			return found
 
 	return null
