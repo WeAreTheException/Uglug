2e4832725f91fae72869200d6f0d580c98311a54
WeAreTheException <trexpronto@gmail.com>
2026-06-10 18:27:46 -0700

    antler fix 1/2

--- Cards/Mutations/MutationPresets/MutationBehaviourHandlers/YAttack.gd
+++ Cards/Mutations/MutationPresets/MutationBehaviourHandlers/YAttack.gd
@@ -5,29 +5,26 @@ class_name YAttack
 @export var attack_right: bool = true
 
 
-func add_attack_events(
+func modify_attack_steps(
 	_runtime: MutationRuntime,
-	events: Array[String]
-) -> void:
+	_steps: Array[AttackStep]
+) -> Array[AttackStep]:
+	var result: Array[AttackStep] = []
+
 	if attack_left:
-		events.append(AttackStep.LEFT)
+		result.append(_make_step(AttackStep.LEFT))
 
 	if attack_right:
-		events.append(AttackStep.RIGHT)
+		result.append(_make_step(AttackStep.RIGHT))
 
+	if result.is_empty():
+		result.append(_make_step(AttackStep.FORWARD))
 
-func add_attack_steps(
-	_runtime: MutationRuntime,
-	steps: Array[AttackStep]
-) -> void:
-	if attack_left:
-		steps.append(_make_step(AttackStep.LEFT))
-
-	if attack_right:
-		steps.append(_make_step(AttackStep.RIGHT))
+	return result
 
 
 func _make_step(direction: String) -> AttackStep:
 	var step := AttackStep.new()
 	step.setup(direction, MutationSource.MUTATION, self)
+
 	return step
