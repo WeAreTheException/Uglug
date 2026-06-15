c84fcb18a6bc571c7900fbf959a7c0a9d383562d
Sergio Lisuardi <13lisuardi@gmail.com>
2026-06-15 02:36:09 -0700

    3 dino mega pose

--- Revamp/PlayPhase/Placement/PlacementHelpers/PlacementRequestPayloadBuilder.gd
+++ Revamp/PlayPhase/Placement/PlacementHelpers/PlacementRequestPayloadBuilder.gd
@@ -21,7 +21,7 @@ func build_payload(
 	return {
 		"owner": owner,
 		"placed_card_runtime_id": placed_card.get_runtime_id(),
-		"target_slot_owner": board.get_owner_of_slot(target_slot),
+		"target_slot_owner": owner,
 		"target_slot_index": target_slot.slot_index,
 		"sacrificed_card_runtime_ids": _get_runtime_ids(sacrificed_cards)
 	}
