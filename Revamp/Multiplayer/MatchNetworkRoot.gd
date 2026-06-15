f7bb5d6ab75cd03f47a18286a165dbb7066cb1c2
Sergio Lisuardi <13lisuardi@gmail.com>
2026-06-14 17:41:16 -0700

    Host/Client should see their own hands

--- Revamp/Multiplayer/MatchNetworkRoot.gd
+++ Revamp/Multiplayer/MatchNetworkRoot.gd
@@ -5,6 +5,7 @@ class_name MatchNetworkRoot
 @export var deck_system_root: DeckSystemRoot
 @export var turn_order_state: MatchTurnOrderState
 @export var match_score_state: MatchScoreState
+@export var print_debug := true
 
 var local_owner: SlotRow.SlotOwner = SlotRow.SlotOwner.PLAYER
 
@@ -35,11 +36,17 @@ func _assign_local_owner() -> void:
 	if turn_order_state != null:
 		turn_order_state.set_controlled_owner(local_owner)
 
-
 func _print_network_status() -> void:
+	if not print_debug:
+		return
+
 	if is_host():
 		print("MATCH NETWORK: HOST")
 		print("LOCAL OWNER: P1")
 	else:
 		print("MATCH NETWORK: CLIENT")
 		print("LOCAL OWNER: P2")
+
+
+func get_local_owner() -> SlotRow.SlotOwner:
+	return local_owner
