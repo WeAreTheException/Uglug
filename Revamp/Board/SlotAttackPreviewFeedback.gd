733909b2b0171c7d643919f8e1ee01fd9b914f5e
WeAreTheException <trexpronto@gmail.com>
2026-07-14 16:03:17 -0700

    Placment working in multplayer with telegraph

--- Revamp/Board/SlotAttackPreviewFeedback.gd
+++ Revamp/Board/SlotAttackPreviewFeedback.gd
@@ -1,15 +1,10 @@
 extends Node
 class_name SlotAttackPreviewFeedback
 
-@export var target: CanvasItem
-
-@export var preview_color: Color = Color(1.0, 0.3, 0.3, 0.55)
-@export var idle_color: Color = Color(1.0, 1.0, 1.0, 0.75)
-
-@export var tween_time: float = 0.08
+@export var shader_rect: CanvasItem
+@export var icon_sprite: CanvasItem
 
 var slot_feedback: SlotFeedback = null
-var tween: Tween = null
 var is_previewed: bool = false
 
 
@@ -24,20 +19,8 @@ func set_previewed(value: bool) -> void:
 
 	is_previewed = value
 
-	if is_previewed:
-		_apply_color(preview_color)
-	else:
-		_apply_color(idle_color)
+	if shader_rect != null:
+		shader_rect.visible = value
 
-
-func _apply_color(color: Color) -> void:
-	if target == null:
-		return
-
-	if tween != null:
-		tween.kill()
-
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(target, "modulate", color, tween_time)
+	if icon_sprite != null:
+		icon_sprite.visible = value
