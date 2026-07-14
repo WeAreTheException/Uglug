2c84321349bca0897b5bb201ac4e85effd8dfbbc
WeAreTheException <trexpronto@gmail.com>
2026-07-14 16:37:01 -0700

    Placementphaseslotsqwuiggle

--- Revamp/Board/SlotPlayPhaseFeedback.gd
+++ Revamp/Board/SlotPlayPhaseFeedback.gd
@@ -1,12 +1,11 @@
 extends Node
 class_name SlotPlayPhaseFeedback
 
-@export var target: CanvasItem
-@export var debug_toggle_key: Key = KEY_R
-@export var enable_debug_toggle: bool = true
+@export var normal_sprite: CanvasItem
+@export var playable_shader_sprite: CanvasItem
 
 var slot_feedback: SlotFeedback = null
-var is_active: bool = false
+var is_playable: bool = false
 
 
 func setup(source_feedback: SlotFeedback) -> void:
@@ -14,31 +13,23 @@ func setup(source_feedback: SlotFeedback) -> void:
 	show_idle()
 
 
-func _input(event: InputEvent) -> void:
-	if not enable_debug_toggle:
-		return
-
-	if event is InputEventKey and event.pressed and not event.echo:
-		if event.keycode == debug_toggle_key:
-			set_active(not is_active)
-
-
 func show_idle() -> void:
-	set_active(false)
+	set_playable(false)
 
 
 func show_playable() -> void:
-	set_active(true)
+	set_playable(true)
 
 
 func show_inactive() -> void:
-	set_active(false)
+	set_playable(false)
 
 
-func set_active(value: bool) -> void:
-	is_active = value
+func set_playable(value: bool) -> void:
+	is_playable = value
 
-	if target == null:
-		return
+	if normal_sprite != null:
+		normal_sprite.visible = not value
 
-	target.visible = value
+	if playable_shader_sprite != null:
+		playable_shader_sprite.visible = value
