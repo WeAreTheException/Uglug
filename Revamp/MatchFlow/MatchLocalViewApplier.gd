18ce9f84b2c1c399622afc3e81891dbec3293611
WeAreTheException <trexpronto@gmail.com>
2026-08-06 19:03:11 -0700

    Arrow works

--- Revamp/MatchFlow/MatchLocalViewApplier.gd
+++ Revamp/MatchFlow/MatchLocalViewApplier.gd
@@ -6,6 +6,10 @@ class_name MatchLocalViewApplier
 @export var player_one_hand: PlayerHandRoot
 @export var player_two_hand: PlayerHandRoot
 
+@export_group("Attack Order Arrows")
+@export var left_to_right_arrow: Sprite2D
+@export var right_to_left_arrow: Sprite2D
+
 @export var print_debug := false
 
 
@@ -24,8 +28,15 @@ func apply_local_view() -> void:
 	else:
 		_show_player_two_view()
 
+	_apply_attack_order_arrows(local_owner)
+
 	if print_debug:
 		print("LOCAL VIEW OWNER: ", _get_owner_name(local_owner))
+
+		if local_owner == SlotRow.SlotOwner.PLAYER:
+			print("LOCAL ATTACK DIRECTION: LEFT TO RIGHT")
+		else:
+			print("LOCAL ATTACK DIRECTION: RIGHT TO LEFT")
 
 
 func _show_player_one_view() -> void:
@@ -38,12 +49,29 @@ func _show_player_two_view() -> void:
 	_set_hand_visible(player_two_hand, true)
 
 
-func _set_hand_visible(hand: PlayerHandRoot, value: bool) -> void:
+func _set_hand_visible(
+	hand: PlayerHandRoot,
+	value: bool
+) -> void:
 	if hand == null:
 		return
 
 	hand.visible = value
 	hand.set_hand_input_enabled(value)
+
+
+func _apply_attack_order_arrows(
+	local_owner: SlotRow.SlotOwner
+) -> void:
+	var is_player_one := (
+		local_owner == SlotRow.SlotOwner.PLAYER
+	)
+
+	if left_to_right_arrow != null:
+		left_to_right_arrow.visible = is_player_one
+
+	if right_to_left_arrow != null:
+		right_to_left_arrow.visible = not is_player_one
 
 
 func _get_owner_name(owner: SlotRow.SlotOwner) -> String:
